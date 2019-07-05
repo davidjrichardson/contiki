65521437c0546278abdf449352f69232558661ce
David Richardson <david@tankski.co.uk>
2019-07-05 11:24:21 +0100

    Clean up workspace

--- tpwsn-rmh-beaconing/control-exp/control-0-0-none-run10/params.js
+++
@@ -1,6 +0,0 @@
-var runNumber = 10;
-var maxFailureCount = 0;
-var failureMode = "none";
-var moteRecoveryDelay = 0;
-var moteFailureProbability = 200;
-var simulationStopTick = 0;
--- tpwsn-rmh-beaconing/control-exp/control-0-0-none-run13/params.js
+++
@@ -1,6 +0,0 @@
-var runNumber = 13;
-var maxFailureCount = 0;
-var failureMode = "none";
-var moteRecoveryDelay = 0;
-var moteFailureProbability = 200;
-var simulationStopTick = 0;
